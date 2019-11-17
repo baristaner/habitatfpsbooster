0c4386f6c0f6612718013bfcd22bc6351813f844
Barış Taner <legendbaris2003@gmail.com>
2019-11-17 20:15:09 +0300

    Update config.lua

--- habitatfpsbooster/lua/baristaner/config.lua
+++ habitatfpsbooster/lua/baristaner/config.lua
@@ -3,15 +3,14 @@ AddCSLuaFile()
 
 --𝐻𝒶𝒷𝒾𝓉𝒶𝓉 𝐹𝒫𝒮 𝐵𝑜𝑜𝓈𝓉𝑒𝓇--
 -----------------------------------------------------------------------------------------------
-habitatfpsbooster.enablefog = true -- [RESTART GEREKTIRIR] büyük harita kullanıyorsan uzağı renderlamaz sis oluşturur bu ayar bazı maplerde sorun çıkarabilir eğer haritana uymuyorsa false yap
+habitatfpsbooster.enablefog = false -- [RESTART GEREKTIRIR] büyük harita kullanıyorsan uzağı renderlamaz sis oluşturur bu ayar bazı maplerde sorun çıkarabilir eğer haritana uymuyorsa false yap
 habitatfpsbooster.fpsboosteractivemsg = "[HabitatFPSBooster] FPS Booster Aktif Donma Olucaktir"
 habitatfpsbooster.fpsboostergerialmsg = "[HabitatFPSBooster] FPS Ayarlari Geriye Donduruldu"
 habitatfpsbooster.fpsboosterinternetmsg = "[HabitatFPSBooster] Internet Optimize Edildi"
 habitatfpsbooster.fpsboostercommand = "!fps" -- FPS BOOSERI AÇMA KOMUTU DEĞİŞTİREBİLİRSİN 
 habitatfpsbooster.fpsboosternpcmodel = "models/humans/group01/female_01.mdl" -- FPS BOOSTER NPCSININ MODELI
 habitatfpsbooster.fpsboosternpcismi = "FPS Arttirici" -- FPS BOOSTERIN NPCSININ USTUNDEKI YAZI
-habitatfpsbooster.fpsboosternpcyaziarkaplanrengi = Color(17, 162, 223, 200) --FPS BOOSTERIN NPCSININ USTUNDEKI RENK 
-habitatfpsbooster.fpsboosterarkaplanrengi = Color(17, 162, 223, 200) -- FPS BOOSTER ARKAPLAN RENGI
+habitatfpsbooster.butonrengi = Color(0,170,204,255)
 
 
 --[[
