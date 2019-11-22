cd25188606a9faecbab0fddc2f56a266f7690757
Barış Taner <legendbaris2003@gmail.com>
2019-11-22 15:04:55 +0300

    Update sv_loadfpsbooster.lua

--- habitatfpsbooster/lua/autorun/server/sv_loadfpsbooster.lua
+++ habitatfpsbooster/lua/autorun/server/sv_loadfpsbooster.lua
@@ -8,3 +8,9 @@ MsgC(Color(255, 255, 0), "[HABITAT FPS BOOSTER]", Color(0, 255, 0), " Enable Fog
 else
 MsgC(Color(255, 255, 0), "[HABITAT FPS BOOSTER]", Color(255, 0, 0), " Enable Fog: Kapali \n")
 end		
+
+if habitatfpsbooster.enableserverfog then
+MsgC(Color(255, 255, 0), "[HABITAT FPS BOOSTER]", Color(0, 255, 0), " Enable Server Fog: Aktif \n")
+else
+MsgC(Color(255, 255, 0), "[HABITAT FPS BOOSTER]", Color(255, 0, 0), " Enable Server Fog: Kapali \n")
+end		
